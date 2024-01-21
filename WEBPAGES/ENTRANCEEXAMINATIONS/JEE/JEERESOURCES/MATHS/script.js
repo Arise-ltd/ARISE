903ae5bf5f697516f91d0b9c231a5238790ffc06
A-R-I-S-E-ltd <157282725+A-R-I-S-E-ltd@users.noreply.github.com>
2024-01-21 16:58:34 +0530

    Add files via upload

---
+++ WEBPAGES/ENTRANCEEXAMINATIONS/JEE/JEERESOURCES/MATHS/script.js
@@ -0,0 +1,5 @@
+function showSubject(subject) {
+    // You can implement logic to filter reviews based on the selected subject
+    // For simplicity, this example does not include dynamic content filtering
+    alert(`Showing reviews for ${subject}`);
+}
