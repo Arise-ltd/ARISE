89fbf4b1c9e2c57dbd8c58f69dd2b71baaa2ee93
A-R-I-S-E-ltd <157282725+A-R-I-S-E-ltd@users.noreply.github.com>
2024-01-21 16:54:54 +0530

    Add files via upload

---
+++ testimonials.cpp
@@ -0,0 +1,70 @@
+
+#testimonial {
+  background: #222222;
+  background-size: cover;
+  padding: 100px 0;
+  color: #fff;
+}
+#testimonial h2{
+color:#fff;
+}
+#testimonial h4 {
+  color: #fff;
+  margin-bottom: 8px;
+}
+#testimonial small {
+  display: block;
+  margin-bottom: 10px;
+  color: rgba(255, 255, 255, 0.7);
+}
+#testimonial .btns {
+  margin-top: 10px;
+}
+
+
+.testimonial-area{
+	padding-bottom:60px;
+}
+.single-testimonial{
+	margin-bottom:30px;
+}
+.single-testimonial blockquote{
+	padding:15px 20px;
+	margin-bottom: 22px;
+	position:relative;
+	-webkit-transition:all .5s ease;
+	-moz-transition:all .5s ease;
+	transition:all .5s ease;
+	font-size: 14px;
+	background: rgba(52, 52, 52, 1);
+
+	color: #919191;
+line-height: 22px;
+
+	border-radius: 40px 10px 10px 0;
+	border: 0;
+}
+ 
+.single-testimonial blockquote:after{
+	/* position:absolute; */
+	/* bottom:-19px; */
+	/* left:45px; */
+	content:"";
+	/* border-top: 19px solid rgba(255, 255, 255, 0.29); */
+
+	/* border-right: 22px solid transparent; */
+}
+.single-testimonial img{
+	border-radius: 0px 50% 50%;
+}
+.single-testimonial h5{
+	padding: 8px 0 0px;
+	font-size: 16px;
+	color: #48B1DF;
+}
+
+
+
+
+
+
